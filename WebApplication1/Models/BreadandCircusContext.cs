a2974c8c31215231007fa72352d851039fd4958d
Benjamin Sprenger <115416353+azrazel95@users.noreply.github.com>
2023-06-10 20:12:55 -0400

    messing with context

--- WebApplication1/Models/BreadandCircusContext.cs
+++ WebApplication1/Models/BreadandCircusContext.cs
@@ -2,17 +2,45 @@
 using System;
 using System.Collections.Generic;
 using Microsoft.EntityFrameworkCore;
+using WebApplication1.Models;
 namespace WebApplication1.Models
 {
     public class BreadandCircusContext : DbContext
     {
         public string ConnectionString { get; set; }
-        public BreadandCircusContext(string connectionString ) {
+        public BreadandCircusContext(string connectionString)
+        {
             this.ConnectionString = connectionString;
         }
         private MySqlConnection GetConnection()
         {
             return new MySqlConnection(ConnectionString);
+        }
+
+        public List<Comment> GetAllComments()
+        {
+            List<Comment> list = new List<Comment>();
+
+            using (MySqlConnection conn = GetConnection())
+            {
+                conn.Open();
+                MySqlCommand cmd = new MySqlCommand("select * from Comment", conn);
+                using (var reader = cmd.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        list.Add(new Comment()
+                        {
+                            Id = Convert.ToInt32(reader["ID"]),
+                            description = reader["description"].ToString(),
+                            date_created = Convert.ToDateTime(reader["date_created"])
+                            // I must figure out how to add the relation between user and post
+                        });
+                    }
+                }
+
+            }
+            return BreadandCircusContext.Comment;
         }
     }
 }
--- WebApplication1/Models/Comment.cs
+++ WebApplication1/Models/Comment.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.AspNetCore.Http.Features;
+using Microsoft.Extensions.Hosting;
+
 
 namespace WebApplication1.Models
 {
@@ -6,16 +8,19 @@ namespace WebApplication1.Models
     {
         private BreadandCircusContext context;
 
-        public int Id
-        { get; set; }
+        public int Id{ get; set; }
 
         public string description { get; set; }
 
         public DateTime date_created {  get; set; }
 
         public int user_id { get; set; }
+        public User User { get; set; }
         // references model user key id
         public int post_id { get; set; }
+        public Post Post { get; set; }
         //references model post key id
     }
+   
+  
 }
